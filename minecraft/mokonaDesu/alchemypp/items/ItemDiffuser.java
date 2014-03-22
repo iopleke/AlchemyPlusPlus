7277b430113e9b7bb74fe677ae3f3262b21f01fc
jakimfett <jakimfett@picraft.com>
2014-03-22 00:08:03 -0700

    Removing diffuser placement code (just until diffuser mechanics are fixed)

--- minecraft/mokonaDesu/alchemypp/items/ItemDiffuser.java
+++ minecraft/mokonaDesu/alchemypp/items/ItemDiffuser.java
@@ -1,30 +1,9 @@
 package mokonaDesu.alchemypp.items;
 
-import mokonaDesu.alchemypp.blocks.BlockRegistry;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeDirection;
-
 public class ItemDiffuser extends APPItem {
 
     public ItemDiffuser(int id, String iconName) {
         super(id, iconName);
         this.maxStackSize = 1;
     }
-
-    @Override
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world,
-            int x, int y, int z, int par7, float par8, float par9, float par10) {
-        if (world.isBlockSolidOnSide(x, y, z, ForgeDirection.UP)
-                && world.isAirBlock(x, y + 1, z)) {
-            world.setBlock(x, y + 1, z,
-                    BlockRegistry.appBlockDistillery.blockID);
-            stack.stackSize--;
-            return true;
-        } else
-            return false;
-
-    }
-
 }
