a3d794123a25ce3a818d765ae04a6714c57634f1
jakimfett <jakimfett@picraft.com>
2014-10-26 22:00:02 -0700

    Fix code whitespace/formatting

--- src/main/java/alchemyplusplus/reference/Naming.java
+++ src/main/java/alchemyplusplus/reference/Naming.java
@@ -2,23 +2,23 @@ package alchemyplusplus.reference;
 
 public class Naming
 {
-    public static final class Blocks
-    {
-        public static final String DIFFUSER = "diffuser";
-        public static final String DISTILLERY = "distillery";
-        public static final String FLESH = "fleshBlock";
-        public static final String FLUID_MIXER = "fluidMixer";
-        public static final String POTION_JUG ="potionJug";
-    }
+	public static final class Blocks
+	{
+		public static final String DIFFUSER = "diffuser";
+		public static final String DISTILLERY = "distillery";
+		public static final String FLESH = "fleshBlock";
+		public static final String FLUID_MIXER = "fluidMixer";
+		public static final String POTION_JUG = "potionJug";
+	}
 
-    public static final class Items
-    {
-        public static final String CONFUSION_POTION = "confusionPotion";
-        public static final String FISH_OIL = "fishOil";
-        public static final String IRON_POWDER = "ironPowder";
-        public static final String MIXING_FILTER = "mixingFilter";
-        public static final String SPRINGY_CORD = "springyCord";
-        public static final String SQUID_EYE = "squidEye";
-        public static final String WOOD_ALCOHOL = "woodAlcohol";
-    }
+	public static final class Items
+	{
+		public static final String CONFUSION_POTION = "confusionPotion";
+		public static final String FISH_OIL = "fishOil";
+		public static final String IRON_POWDER = "ironPowder";
+		public static final String MIXING_FILTER = "mixingFilter";
+		public static final String SPRINGY_CORD = "springyCord";
+		public static final String SQUID_EYE = "squidEye";
+		public static final String WOOD_ALCOHOL = "woodAlcohol";
+	}
 }
