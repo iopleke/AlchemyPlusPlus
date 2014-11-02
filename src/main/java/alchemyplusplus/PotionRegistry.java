2cb7f641b80f12c3c81037e54b1e32a31c5a93f4
jakimfett <jakimfett@picraft.com>
2014-11-02 11:37:39 -0800

    Add debug for fluid registration

--- src/main/java/alchemyplusplus/PotionRegistry.java
+++ src/main/java/alchemyplusplus/PotionRegistry.java
@@ -4,6 +4,7 @@ import alchemyplusplus.gui.CreativeTab;
 import alchemyplusplus.potion.PotionFluid;
 import alchemyplusplus.potion.PotionFluidBlock;
 import alchemyplusplus.potion.PotionFluidBlockItem;
+import alchemyplusplus.reference.Settings;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.material.Material;
 import net.minecraft.potion.Potion;
@@ -20,6 +21,11 @@ public class PotionRegistry
 			{
 				Fluid potionFluid = new PotionFluid(pot);
 				FluidRegistry.registerFluid(potionFluid);
+
+				if (Settings.DebugMode)
+				{
+					AlchemyPlusPlus.LOGGER.info("Registered fluid for " + pot.getName());
+				}
 
 				PotionFluidBlock block = new PotionFluidBlock(potionFluid, Material.water, pot);
 				block.setCreativeTab(CreativeTab.APP_TAB);
