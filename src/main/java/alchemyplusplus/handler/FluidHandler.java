f0f41885bd4dceb464f8ddc219881ccec4650f20
jakimfett <jakimfett@gmail.com>
2014-12-11 08:46:47 -0800

    Fix code whitespace/formatting

--- src/main/java/alchemyplusplus/handler/FluidHandler.java
+++ src/main/java/alchemyplusplus/handler/FluidHandler.java
@@ -4,22 +4,18 @@ import alchemyplusplus.item.PotionBucket;
 import alchemyplusplus.potion.fluid.PotionFluidBlock;
 import alchemyplusplus.reference.Settings;
 import cpw.mods.fml.common.eventhandler.Event;
-import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import java.util.HashMap;
 import java.util.Map;
 import net.minecraft.block.Block;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemGlassBottle;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
-import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 
 public class FluidHandler
 {
@@ -128,12 +124,15 @@ public class FluidHandler
                 int x = event.x + dir.offsetX;
                 int y = event.y + dir.offsetY;
                 int z = event.z + dir.offsetZ;
-                if (!event.entityPlayer.canPlayerEdit(x,y,z,event.face,equippedItem)) return;
-                Block potentialFluidBlock = event.world.getBlock(x,y,z);
+                if (!event.entityPlayer.canPlayerEdit(x, y, z, event.face, equippedItem))
+                {
+                    return;
+                }
+                Block potentialFluidBlock = event.world.getBlock(x, y, z);
                 if (potentialFluidBlock instanceof PotionFluidBlock)
                 {
                     PotionFluidBlock potionFluidBlock = (PotionFluidBlock) potentialFluidBlock;
-                    if (potionFluidBlock.isSourceBlock(event.world,x,y,z))
+                    if (potionFluidBlock.isSourceBlock(event.world, x, y, z))
                     {
                         Potion fluidPotion = potionFluidBlock.getFluidStackPotion();
                         if (fluidPotion != null)
@@ -141,13 +140,15 @@ public class FluidHandler
                             int potionTier = 0; //0 for T1, 1 for T2
                             int potionExtended = 0; //1 for true
                             int potionSplash = 1; //1 for drinkable, 2 for splash
-                            int potionDamage = fluidPotion.id + (potionTier<<5) + (potionExtended<<6) + (potionSplash<<13);
+                            int potionDamage = fluidPotion.id + (potionTier << 5) + (potionExtended << 6) + (potionSplash << 13);
                             ItemStack potionStack = new ItemStack(Items.potionitem, 1, potionDamage);
                             if (!event.entityPlayer.capabilities.isCreativeMode)
                             {
                                 event.entityPlayer.inventory.decrStackSize(event.entityPlayer.inventory.currentItem, 1);
-                                if (Settings.consumeSourceBlocks && event.world.rand.nextFloat()<Settings.consumeSourceBlocksChance)
-                                    event.world.setBlockToAir(x,y,z);
+                                if (Settings.consumeSourceBlocks && event.world.rand.nextFloat() < Settings.consumeSourceBlocksChance)
+                                {
+                                    event.world.setBlockToAir(x, y, z);
+                                }
                             }
                             if (!event.entityPlayer.inventory.addItemStackToInventory(potionStack))
                             {
@@ -155,7 +156,10 @@ public class FluidHandler
                             }
                         }
                     }
-                    if (event.world.isRemote) return;
+                    if (event.world.isRemote)
+                    {
+                        return;
+                    }
                     event.setCanceled(true);
                 }
             }
