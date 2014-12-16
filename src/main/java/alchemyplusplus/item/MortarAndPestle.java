6d053d34bacccf68dd5ea5ac0846061ab9d079dd
jakimfett <jakimfett@gmail.com>
2014-12-15 20:47:52 -0800

    Overwrite with @hilburn's changes

--- src/main/java/alchemyplusplus/item/MortarAndPestle.java
+++ src/main/java/alchemyplusplus/item/MortarAndPestle.java
@@ -11,6 +11,7 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.StatCollector;
@@ -21,7 +22,6 @@ public class MortarAndPestle extends ItemBasic
 
     private IIcon[] icons;
     private IIcon filledIcon;
-    public boolean isFilled = false;
 
     public MortarAndPestle(String itemname)
     {
@@ -35,36 +35,33 @@ public class MortarAndPestle extends ItemBasic
     @Override
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
-        if (stack != null)
+        if (!world.isRemote)
         {
-            if (!world.isRemote)
+            MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(world, player, true);
+
+            if (movingobjectposition != null)
             {
-                MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(world, player, true);
 
-                if (movingobjectposition != null)
+                if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
                 {
+                    int i = movingobjectposition.blockX;
+                    int j = movingobjectposition.blockY;
+                    int k = movingobjectposition.blockZ;
 
-                    if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+                    Material material = world.getBlock(i, j, k).getMaterial();
+                    int l = world.getBlockMetadata(i, j, k);
+
+                    if (material == Material.water && l == 0)
                     {
-                        int i = movingobjectposition.blockX;
-                        int j = movingobjectposition.blockY;
-                        int k = movingobjectposition.blockZ;
 
-                        Material material = world.getBlock(i, j, k).getMaterial();
-                        int l = world.getBlockMetadata(i, j, k);
+                        this.setFilled(true, stack);
+                        return stack;
 
-                        if (material == Material.water && l == 0)
-                        {
-
-                            this.setFilled(true, stack);
-                            return stack;
-
-                        }
                     }
                 }
             }
-            this.setFilled(false, stack);
         }
+        this.setFilled(false, stack);
         return stack;
     }
 
@@ -83,38 +80,39 @@ public class MortarAndPestle extends ItemBasic
 
     public void setFilled(boolean state, ItemStack stack)
     {
-        if (stack.getTagCompound() != null)
+        if (!stack.hasTagCompound())
         {
-            stack.getTagCompound().setBoolean("Filled", state);
+            stack.setTagCompound(new NBTTagCompound());
         }
+        stack.getTagCompound().setBoolean("Filled", state);
     }
 
     public boolean getFilled(ItemStack stack)
     {
-        if (stack.getTagCompound() != null)
+        if (!stack.hasTagCompound())
         {
-            return stack.getTagCompound().getBoolean("Filled");
+            return false;
         }
-        return false;
+        return stack.getTagCompound().getBoolean("Filled");
     }
 
-    public final void toggleFilled()
+    public final void toggleFilled(ItemStack stack)
     {
-        this.isFilled = !this.isFilled;
+        setFilled(!getFilled(stack), stack);
     }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public IIcon getIconFromDamageForRenderPass(int damage, int pass)
+    public IIcon getIcon(ItemStack stack, int pass)
     {
         if (pass == 1)
         {
-            if (this.isFilled)
+            if (getFilled(stack))
             {
                 return this.filledIcon;
             }
         }
-        return icons[damage];
+        return icons[stack.getItemDamage()];
     }
 
     @Override
