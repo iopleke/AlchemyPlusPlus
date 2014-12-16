00024dc2f5a8f0ce243fe119d7552b2c11db8936
jakimfett <jakimfett@picraft.com>
2014-12-15 18:07:56 -0800

    Add color constants

--- src/main/java/alchemyplusplus/reference/Naming.java
+++ src/main/java/alchemyplusplus/reference/Naming.java
@@ -25,4 +25,25 @@ public class Naming
         public static final String WOOD_ALCOHOL = "woodAlcohol";
         public static final String MortarAndPestle = "mortarAndPestle";
     }
+
+    public static final class Colors
+    {
+        public static final String Black = "\u00A70";
+        public static final String DarkBlue = "\u00A71";
+        public static final String DarkGreen = "\u00A72";
+        public static final String DarkCyan = "\u00A73";
+        public static final String DarkRed = "\u00A74";
+        public static final String Purple = "\u00A75";
+        public static final String Orange = "\u00A76";
+        public static final String LightGrey = "\u00A77";
+        public static final String DarkGrey = "\u00A78";
+        public static final String LightBlue = "\u00A79";
+        public static final String LightGreen = "\u00A7a";
+        public static final String LightCyan = "\u00A7b";
+        public static final String LightRed = "\u00A7c";
+        public static final String Pink = "\u00A7d";
+        public static final String Yellow = "\u00A7e";
+        public static final String White = "\u00A7f";
+
+    }
 }
