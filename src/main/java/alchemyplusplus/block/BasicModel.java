6c1a603f3a7bccc536fc30e1f00c2482e100e08b
jakimfett <jakimfett@picraft.com>
2015-03-22 16:55:16 -0700

    Refactor to use JakimBox

--- src/main/java/alchemyplusplus/block/BasicModel.java
+++
@@ -1,8 +0,0 @@
-package alchemyplusplus.block;
-
-import net.minecraft.client.model.ModelBase;
-
-public abstract class BasicModel extends ModelBase
-{
-    public abstract void render(float rotation);
-}
