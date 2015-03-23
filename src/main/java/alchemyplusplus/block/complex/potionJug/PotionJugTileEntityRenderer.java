8523deb0c7009daac368ed0fbaf665ed3b78de9b
jakimfett <jakimfett@picraft.com>
2015-03-22 21:23:41 -0700

    Remove cast to PotionJugModel

--- src/main/java/alchemyplusplus/block/complex/potionJug/PotionJugTileEntityRenderer.java
+++ src/main/java/alchemyplusplus/block/complex/potionJug/PotionJugTileEntityRenderer.java
@@ -9,8 +9,6 @@ import org.lwjgl.opengl.GL11;
 public class PotionJugTileEntityRenderer extends BasicTileEntityRenderer
 {
 
-    PotionJugModel model;
-
     public PotionJugTileEntityRenderer()
     {
         super(1.0F, 0.0625F);
@@ -23,11 +21,11 @@ public class PotionJugTileEntityRenderer extends BasicTileEntityRenderer
     {
         if (model instanceof PotionJugModel && tileEntity instanceof PotionJugTileEntity)
         {
-            model.percentage = ((float) ((PotionJugTileEntity) tileEntity).containerHas) / ((PotionJugTileEntity) tileEntity).containerMax;
-            if (model.percentage > 0)
+            ((PotionJugModel) model).percentage = ((float) ((PotionJugTileEntity) tileEntity).containerHas) / ((PotionJugTileEntity) tileEntity).containerMax;
+            if (((PotionJugModel) model).percentage > 0)
             {
                 // @TODO - use this for the diffuser color
-                model.color = PotionHelper.func_77915_a(((PotionJugTileEntity) tileEntity).potionID, false);
+                ((PotionJugModel) model).color = PotionHelper.func_77915_a(((PotionJugTileEntity) tileEntity).potionID, false);
             }
         }
     }
@@ -36,14 +34,14 @@ public class PotionJugTileEntityRenderer extends BasicTileEntityRenderer
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float f)
     {
         GL11.glPushMatrix();
-        GL11.glTranslatef((float) x, (float) y, (float) z);
+        GL11.glTranslated(x + xOffset, y + yOffset, z + zOffset);
 
         GL11.glPushMatrix();
         GL11.glTranslatef(0.5F, 0, 0.5F);
         GL11.glRotatef(tileEntity.getBlockMetadata() * (-90F), 0F, 1F, 0F);
         GL11.glTranslatef(-0.5F, 0, -0.5F);
 
-        bindTexture(Textures.Model.POTION_JUG);
+        bindTexture(texture);
 
         modelSpecificOperations(tileEntity);
 
